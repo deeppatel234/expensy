c9e6dacdf4e09e9f2ec4b21058ff29d2c5ea3588
Deep Patel <deeppatel234@gmail.com>
2019-04-14 12:47:40 +0530

    create multiple records

--- mixins/AccessControl.js
+++ mixins/AccessControl.js
@@ -1,5 +1,6 @@
 const { Fields, getObjectID } = require('mongoorm');
 const _pick = require('lodash/pick');
+const _keyBy = require('lodash/keyBy');
 const _intersection = require('lodash/intersection');
 
 
@@ -75,6 +76,11 @@ const AccessControl = ClassName => class extends ClassName {
       route: '/create',
       method: this.create,
       params: ['record'],
+      auth: !this.hasPublicAccess(OPERATIONS.CREATE),
+    }, {
+      route: '/createmany',
+      method: this.createMany,
+      params: ['records'],
       auth: !this.hasPublicAccess(OPERATIONS.CREATE),
     }, {
       route: '/read',
@@ -381,6 +387,38 @@ const AccessControl = ClassName => class extends ClassName {
     const newRecord = this.createRecord(dataToCreate);
     await newRecord.save();
     return newRecord.get();
+  }
+
+  /**
+   * Create multiple record
+   *
+   * @param {object} value
+   * @param {object} user
+   */
+  async createMany({ records }, context) {
+    let datasToCreate = await Promise.all(records.map(record => this.hasAccess(OPERATIONS.CREATE, { record }, context)));
+
+    datasToCreate.forEach((dc) => {
+      if (dc._id) {
+        dc.mid = dc._id;
+        delete dc._id;
+      }
+    });
+
+    datasToCreate = _keyBy(datasToCreate, 'mid');
+
+    const response = [];
+
+    const mids = Object.keys(datasToCreate);
+
+    for (let i = 0; i < mids.length; i++) {
+      const mid = mids[i];
+      const newRecord = this.createRecord(datasToCreate[mid]);
+      await newRecord.save();
+      response.push({ ...newRecord.get(), mid });
+    }
+
+    return response;
   }
 
   /**
